96ec222a4bcbbe17c5f6367ffbd7b68fea31ef3b
Jijo Daniel <jijodaniel95@gmail.com>
2025-07-11 15:51:31 -0400

    Removed db config

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require_relative "boot"
 
-require "rails/all"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "active_record/railtie" # Removed to disable database
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
