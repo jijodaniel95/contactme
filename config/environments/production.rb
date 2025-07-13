35c091d96855c012bbc2a9c9f6b555fbe67943b8
Jijo Daniel <jijodaniel95@gmail.com>
2025-07-13 12:46:57 -0400

    Updated prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.i18n.fallbacks = true
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
